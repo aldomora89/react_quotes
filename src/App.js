384e47e247e2de82ee350237e7a0db55427282bb
AldoMora89 <aldomora89@outlook.com>
2023-06-06 23:55:18 -0600

    Fix with new url quote random source

--- src/App.js
+++ src/App.js
@@ -1,59 +1,42 @@
 import './App.css';
 import './Quote.css';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState,useEffect } from 'react';
 
-const App = () => {
-  const randomIndex = (arr) => { // returns a random int value to use as an index
-    return Math.floor(Math.random() * arr.length)
+
+export default function App() {
+const [response, setResponse] = useState(null);
+const quoteAPI = async () => {
+  try {
+    const data = await axios.get("https://dummyjson.com/quotes/random");
+    
+    setResponse(data.data);
+    //console.log("setresponse:",response);
+  } catch (error) {
+    console.log(error);
   }
 
-  const [quote, setQuote] = useState("");
-  const [index, setIndex] = useState(0);
-
-  const quoteAPI = async () => {
-    let arrayOfQuotes = [];
-
-    try {
-      const data = await axios.get("https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/backup/quotes.json");
-      arrayOfQuotes = data.data;
-      //console.log(arrayOfQuotes);
-      const quote = arrayOfQuotes.map((arrayOfQuote) =>
-        <div key={arrayOfQuote.id}>
-          <div className="quote">
-            
-            <h2>"{arrayOfQuote.en}"</h2>
-            
-          </div>
-          <div className="author">
-            {arrayOfQuote.author}
-          </div>
-        </div>);
-      //console.log(quote);
-      setQuote(quote);
-    } catch (error) {
-      console.log(error);
-    }
-
-  };
-
-  useEffect(() => {
-    quoteAPI();
-  }, []);
-
-  return (<div className="App">
-    <div className="quoteBox">
-      <div className="container">
-        <div className="quoteButton">
-          {" "}
-          <button onClick={_ => setIndex(randomIndex(quote))}>Get Random Quote</button>
-        </div>
-        {quote[index]}
-
-      </div>
-    </div>
-  </div>
-  );
 };
 
-export default App;
+useEffect(() => {
+  // Trigger the API Call
+  quoteAPI();
+}, []);
+
+return (<div className="App">
+  <div className="quoteBox">
+    <div className="container">
+      <div className="quoteButton">
+      <h2>"{response && response.quote}"</h2>
+      <p>{response && response.author}</p>
+        <button onClick={() => quoteAPI()}>Get Random Quote</button>
+        
+      </div>
+
+    </div>
+  </div>
+</div>
+);
+};
+
+
